2c0564c84d3a3ee44b449b892002686e59f3f4ca
guripong <guripong@hanmail.net>
2025-06-23 09:41:05 +0900

    fix

--- src/session.ts
+++ src/session.ts
@@ -1,4 +1,15 @@
-export type providerEnum = 'google' | 'naver' | 'kakao';
+// export type providerEnum = 'google' | 'naver' | 'kakao';
+export enum OauthUserType {
+  CHILD = 'child',
+  PARENT = 'parent',
+  ADMIN = 'admin',
+}
+export enum OauthProvider {
+  GOOGLE = 'google',
+  NAVER = 'naver',
+  KAKAO = 'kakao',
+}
+
 export interface CommonSessionData {
   access_token: string;
   access_token_expire: number;
@@ -11,16 +22,17 @@ export interface CommonSessionData {
 
 export interface ParentSessionData extends CommonSessionData {
   user_idx: number;
-  user_logo_url: string; //대표 oauth로 연동된 사진 복사 혹은 변경된 url 임
+  user_logo_url?: string; //대표 oauth로 연동된 사진 복사 혹은 변경된 url 임
   client_id: string; //로긴할때의 클라이언트아이디
   email: string;
   name: string;
-  provider: providerEnum;
+  provider: OauthProvider;
 }
 
 export interface ChildSessionData extends CommonSessionData {
   child_idx: number;
-  child_logo_url: string;
+  child_image_url?: string;
+  child_ID:string;  
 }
 
 export type userSessionData = ParentSessionData | ChildSessionData;
