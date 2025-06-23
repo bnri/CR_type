a2182bea80fb388e6526922bac7c33b4b359736d
guripong <guripong@hanmail.net>
2025-06-23 10:12:44 +0900

    fix

--- dist/session.js
+++ dist/session.js
@@ -1,13 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OauthProvider = exports.OauthUserType = void 0;
 // export type providerEnum = 'google' | 'naver' | 'kakao';
-export var OauthUserType;
+var OauthUserType;
 (function (OauthUserType) {
     OauthUserType["CHILD"] = "child";
     OauthUserType["PARENT"] = "parent";
     OauthUserType["ADMIN"] = "admin";
-})(OauthUserType || (OauthUserType = {}));
-export var OauthProvider;
+})(OauthUserType || (exports.OauthUserType = OauthUserType = {}));
+var OauthProvider;
 (function (OauthProvider) {
     OauthProvider["GOOGLE"] = "google";
     OauthProvider["NAVER"] = "naver";
     OauthProvider["KAKAO"] = "kakao";
-})(OauthProvider || (OauthProvider = {}));
+})(OauthProvider || (exports.OauthProvider = OauthProvider = {}));
