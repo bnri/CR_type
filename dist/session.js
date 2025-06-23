8fd0e5a941e4a63f8e10e297194baded1cff8192
guripong <guripong@hanmail.net>
2025-06-23 10:08:05 +0900

    fix

---
+++ dist/session.js
@@ -0,0 +1,13 @@
+// export type providerEnum = 'google' | 'naver' | 'kakao';
+export var OauthUserType;
+(function (OauthUserType) {
+    OauthUserType["CHILD"] = "child";
+    OauthUserType["PARENT"] = "parent";
+    OauthUserType["ADMIN"] = "admin";
+})(OauthUserType || (OauthUserType = {}));
+export var OauthProvider;
+(function (OauthProvider) {
+    OauthProvider["GOOGLE"] = "google";
+    OauthProvider["NAVER"] = "naver";
+    OauthProvider["KAKAO"] = "kakao";
+})(OauthProvider || (OauthProvider = {}));
