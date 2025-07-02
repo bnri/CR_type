c84a47666e04ad5dab3ca058330a022e7e1979ae
guripong <guripong@hanmail.net>
2025-07-02 12:16:34 +0900

    fix

--- dist/socket/socket-message.types.d.ts
+++ dist/socket/socket-message.types.d.ts
@@ -1,26 +1,30 @@
 import { OauthUserType } from "../session";
 export interface MessageRequest {
+    room_key: string;
     receiver_type: OauthUserType;
     receiver_idx: number;
     msg: string;
+    temp_id?: string;
 }
 export interface MessageResponse {
     msg: string;
     sender_type: OauthUserType;
     sender_idx: number;
-    receiver_type: 'parent' | 'child';
+    receiver_type: OauthUserType;
     receiver_idx: number;
     createdAt: string;
     room_key: string;
     is_read: boolean;
+    _id: string;
+    temp_id?: string;
 }
 export interface ChatMessageRefreshRequest {
     roomKey: string;
     start?: string;
     end?: string;
+    count?: number;
 }
 export interface MessageLogDocument extends MessageResponse {
-    _id?: string;
     createdAt: string;
     updatedAt?: string;
 }
